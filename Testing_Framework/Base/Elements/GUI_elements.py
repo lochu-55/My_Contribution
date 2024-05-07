fede1106d01bee9bb497c7cfe5c592a63fb0d5fa
lochani <lochu5vilehya@gmail.com>
2024-05-07 10:42:07 +0530

    final testing framework

---
+++ Testing_Framework/Base/Elements/GUI_elements.py
@@ -0,0 +1,17 @@
+
+class Elements:
+    APP = "notepad.exe"
+    TEXT = "Welcome to VEDA"
+    FILE_NAME_TO_SAVE = "ele1"
+    FILE_NAME_TO_OPEN = r"C:\Users\vlab\Downloads\ele"
+    FONT = "Comic Sans MS"
+    FONT_STYLE = "Bold"
+    FONT_SIZE = "20"
+    FIND_TEXT = "VEDA"
+    FIND_TEXT_TO_REPLACE = "VEDA"
+    REPLACE_WITH = "SoCtronics"
+    PATH = r"Base\utils\Screenshots\GUI_SS"
+    LOG_PATH2 = r"Base\utils\Logs\GUI_logs\gui.log"
+
+
+
